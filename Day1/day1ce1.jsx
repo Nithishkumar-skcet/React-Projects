05d459af0ebe5c357fc0932765eb60bb7a87e531
Nithishkumar-skcet <727723eucs152@skcet.ac.in>
2024-08-03 14:02:00 +0530

    Add files via upload

---
+++ Day1/day1ce1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const D1CE1=()=>{
+    return(
+        
+        alert("Message from Javascript Alert!"),
+        console.log("Message to developer")
+    );
+
+};
+export default D1CE1;
